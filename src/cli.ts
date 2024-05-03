0bef8b578756b9b78b8bf139c7d77b65db506649
hywax <me@hywax.space>
2024-05-03 22:47:51 +0500

    chore: cli args list

--- src/cli.ts
+++ src/cli.ts
@@ -9,6 +9,28 @@ const main = defineCommand({
     version,
     description,
   },
+  args: {
+    source: {
+      type: 'string',
+      description: 'Source name to fetch events from (e.g. xmlcalendar)',
+      default: 'xmlcalendar',
+    },
+    year: {
+      type: 'string', // @todo change "year" to number type. Now citty does not support the number type. Support is expected in ^0.2.0 version.
+      description: 'Year to fetch events for',
+      default: new Date().getFullYear().toString(),
+    },
+    path: {
+      type: 'string',
+      description: 'Calendar file name to create or update',
+      default: 'calendar.ics',
+    },
+    output: {
+      type: 'boolean',
+      description: 'Set to true to output the calendar file to stdout without saving it to a file',
+      default: false,
+    },
+  },
   run: () => {
     // @todo implement this method
   },
