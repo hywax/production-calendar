fc19ac937aeba6b0dd805a6011f69c4662bbccc9
hywax <me@hywax.space>
2024-05-04 01:01:45 +0500

    chore!: implements cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 import process from 'node:process'
 import { defineCommand, runMain } from 'citty'
+import consola from 'consola'
 import { description, name, version } from '../package.json'
+import { createCalendar } from './calendar'
+import type { Source, SourceName } from './types'
+import xmlcalendar from './sources/xmlcalendar'
+
+function createSource(source: SourceName, options?: any): Source {
+  switch (source) {
+    case 'xmlcalendar':
+      return xmlcalendar(options)
+    default:
+      throw new Error(`Source "${source}" not found`)
+  }
+}
 
 const main = defineCommand({
   meta: {
@@ -12,27 +25,49 @@ const main = defineCommand({
   args: {
     source: {
       type: 'string',
+      alias: 's',
       description: 'Source name to fetch events from (e.g. xmlcalendar)',
       default: 'xmlcalendar',
     },
     year: {
-      type: 'string', // @todo change "year" to number type. Now citty does not support the number type. Support is expected in ^0.2.0 version.
-      description: 'Year to fetch events for',
-      default: new Date().getFullYear().toString(),
+      type: 'boolean',
+      alias: 'y',
+      description: 'Year to fetch events for. Defaults to the current year',
+      required: false,
     },
-    path: {
+    file: {
       type: 'string',
+      alias: 'f',
       description: 'Calendar file name to create or update',
       default: 'calendar.ics',
     },
+    params: {
+      type: 'string',
+      alias: 'p',
+      description: 'Source specific parameters. Should be a JSON string',
+      default: '{}',
+    },
     output: {
       type: 'boolean',
+      alias: 'o',
       description: 'Set to true to output the calendar file to stdout without saving it to a file',
       default: false,
     },
   },
-  run: () => {
-    // @todo implement this method
+  run: async ({ args }) => {
+    const source = createSource(args.source, JSON.parse(args.params))
+    const calendar = await createCalendar({
+      source,
+      year: Number(args.year) || new Date().getFullYear(),
+    })
+
+    if (args.output) {
+      consola.log(calendar.getIcs())
+      process.exit(0)
+    }
+
+    await calendar.saveToFile(args.file)
+    consola.success('Calendar saved to `%s`', args.path)
   },
 })
 
