8bd1ca0959186c355b85e27635a3a2b06dc36305
hywax <me@hywax.space>
2024-05-03 20:58:54 +0500

    chore: export index

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export * from './types'
+export * from './calendar'
+export { defineSource } from './sources/utils'
+
+export const builtinSources = {
+  xmlcalendar: 'production-calendar/sources/xmlcalendar',
+} as const
+
+type ExtractOptions<T> = T extends (opts: infer Options) => any ? Options : never
+
+export interface BuiltinSourceOptions {
+  xmlcalendar: ExtractOptions<
+    (typeof import('./sources/xmlcalendar'))['default']
+  >
+}
+
+export type BuiltinSourceName = keyof typeof builtinSources
